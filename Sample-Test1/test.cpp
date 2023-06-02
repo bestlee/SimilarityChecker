430551bc6c710958a2b8896373501e9bb30016ee
Myunghee <audgml0087@naver.com>
2023-06-02 14:20:12 +0900

    LENGTH 1차 Enable

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,26 @@
 #include "pch.h"
 #include "../SimilarityCheck/Checker.cpp"
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+TEST(TC_Length, InvalidArgument)
+{
+	EXPECT_THROW(Checker().CheckSimirality("","ABC"), std::invalid_argument);
+	EXPECT_THROW(Checker().CheckSimirality("ABC",""), std::invalid_argument);
+	EXPECT_THROW(Checker().CheckSimirality("123", "ABC"), std::invalid_argument);
+	EXPECT_THROW(Checker().CheckSimirality("ABC", "123"), std::invalid_argument);
+	EXPECT_THROW(Checker().CheckSimirality("abc", "ABC"), std::invalid_argument);
+}
+
+TEST(TC_Length, PerfectMatch)
+{
+	EXPECT_EQ(MAX_LENGTH_SCORE, Checker().CheckSimirality("ABC", "ABC"));
+	EXPECT_EQ(MAX_LENGTH_SCORE, Checker().CheckSimirality("ABCDE", "ABCDE"));
+	EXPECT_EQ(MAX_LENGTH_SCORE, Checker().CheckSimirality("ABCDEF", "ABCDEF"));
+}
+
+TEST(TC_Length,lengthMisMatch)
+{
+	EXPECT_EQ(0, Checker().CheckSimirality("A", "BB"));
+	EXPECT_EQ(MAX_LENGTH_SCORE, Checker().CheckSimirality("ASD", "DSA"));
+	EXPECT_EQ(20, Checker().CheckSimirality("AAABB", "BAA"));
+	EXPECT_EQ(30, Checker().CheckSimirality("AA", "AAA"));
 }
--- SimilarityCheck/Checker.cpp
+++ SimilarityCheck/Checker.cpp
@@ -1,5 +1,74 @@
-class checker
+#include <stdexcept>
+#include <string>
+
+#define  MAX_LENGTH_SCORE (60)
+class Checker
 {
 public:
+	int CheckSimirality(const std::string str, const std::string str2)
+	{
+		int result = 0;
 
+		ArgValidCheck(str, str2);
+
+		result += checkLength(str, str2);
+		result += checkAlpha(str, str2);
+		return result;
+	}
+private:
+	void ArgValidCheck(const std::string str, const std::string str2)
+	{
+		if (str == "" || str2 == "")
+		{
+			throw std::invalid_argument("값을 넣어야함");
+		}
+
+		for (char c : str)
+		{
+			if (c < 'A' || c > 'Z')
+			{
+				throw std::invalid_argument("Only upper Alphabet");
+			}
+		}
+		for (char c : str2)
+		{
+			if (c < 'A' || c > 'Z')
+			{
+				throw std::invalid_argument("Only upper Alphabet");
+			}
+		}
+	}
+
+	int CalcLengthPoint(int longStrLength, int shortStrLength)
+	{
+		if (longStrLength == shortStrLength) return MAX_LENGTH_SCORE;
+		else if (longStrLength >= (shortStrLength * 2)) return 0;
+
+		return (1 - ((longStrLength - shortStrLength) / (double)shortStrLength)) * MAX_LENGTH_SCORE;;
+	}
+
+	int checkLength(const std::string str, const std::string str2)
+	{
+		int lengthCheck = 0;
+		std::string longStr = str;
+		std::string shortStr = str2;
+
+		if (str.length() < str2.length())
+		{
+			longStr = str2;
+			shortStr = str;
+		}
+
+		int longStrLength = longStr.length();
+		int shortStrLength = shortStr.length();
+
+		return CalcLengthPoint(longStrLength, shortStrLength);
+	}
+
+	int checkAlpha(const std::string str, const std::string str2)
+	{
+		int alphacheck = 0;
+
+		return alphacheck;
+	}
 };
